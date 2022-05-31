5ee7dccea82b16f577e60c6b36470a28f322ce52
TommyBerkmortel <B_tommy_@hotmail.com>
2022-05-31 20:26:03 +0200

    opdracht 1 klaar nu

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import './App.css';
+import Header from "./Components/header/Header";
 
+
+function App() {
+    return (
+        <div className="page-container">
+            <Header />
+
+            <h1>Hallo</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur.</p>
+        </div>
+    );
+}
+
+export default App;
