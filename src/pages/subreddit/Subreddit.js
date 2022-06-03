2afb4d4ba10b3dfb442234ee55a355bbe66d264e
TommyBerkmortel <B_tommy_@hotmail.com>
2022-06-03 12:07:32 +0200

    Opdracht 4: proberen dynamisch te maken

--- src/pages/subreddit/Subreddit.js
+++ src/pages/subreddit/Subreddit.js
@@ -3,15 +3,15 @@ import {useParams} from "react-router-dom";
 import axios from "axios";
 
 function Subreddit() {
-    /*const { id } = useParams();
-    console.log(id);*/
+    const { id } = useParams();
 
-    const [reddits, setReddits] = useState([]);
+    const [reddits, setReddits] = useState({});
 
     useEffect(() => {
         async function getRedditData() {
             try {
-                const result = await axios.get(`https://www.reddit.com/r/memes/about.json`)
+                /*const result = await axios.get(`https://www.reddit.com/r/memes/about.json`)*/
+                const result = await axios.get(`https://www.reddit.com/r/${id}/about.json`)
                 console.log(result.data.data)
                 setReddits(result.data.data)
 
@@ -27,14 +27,16 @@ function Subreddit() {
 
         <div className="outer-container">
             <div className="inner-container">
-                <h1>Subreddit</h1>
-                <h3>Title</h3>
-                <p>{reddits.title}</p>
-                <h3>Description</h3>
-                <p>{reddits.public_description}</p>
-                <h3>Number of subscribers</h3>
-                <p>{reddits.subscribers}</p>
-
+                {Object.keys(reddits).length > 0 && (
+                    <div className="subReddit">
+                        <h1>Subreddit</h1>
+                        <h3>Title</h3>
+                        <p>{reddits.title}</p>
+                        <h3>Description</h3>
+                        <p>{reddits.public_description}</p>
+                        <h3>Number of subscribers</h3>
+                        <p>{reddits.subscribers}</p>
+                    </div>)}
             </div>
         </div>
     );
